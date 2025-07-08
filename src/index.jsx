c727cb5748cbe5372306af0795e23e644e11b5f6
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-07-08 18:28:42 +0530

    erased default code

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import style from "./styles/Index.module.scss";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <>
+      <div className={style.div}>
+        <App />
+      </div>
+    </>
+  </StrictMode>
+);
