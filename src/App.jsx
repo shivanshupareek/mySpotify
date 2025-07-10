0fb3750048f0ea0c958fb00bfda68656fa21970a
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-07-10 20:13:35 +0530

    updated all existing icons and added new icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import style from "./styles/App.module.scss";
 import "./styles/_global.scss";
+// import add from "./assets/add.svg";
+// import connectDevice from "./assets/connect_device.svg";
 
 function App() {
   return (
     <>
       <div className={style.div}>
         <h1>Hello, World!</h1>
+        {/* <img src={add} alt="addition button" />
+        <img src={connectDevice} alt="icon for connecting devices" /> */}
       </div>
     </>
   );
