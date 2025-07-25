4ce35626941ee28745d3d0010c7f65c5edc0f797
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-07-25 09:30:04 +0530

    created reusable component for all icon functions

---
+++ src/logic/IconFunction.jsx
@@ -0,0 +1,40 @@
+import style from  "../styles/IconFunction.module.scss"
+import {useState} from "react";
+import Icon from "./Icon.jsx";
+import Tooltip from "./Tooltip.jsx";
+
+function IconFunction({
+                          label,
+                          path,
+                          width,
+                          height,
+                          hColor,
+                          color,
+                          onClick,
+                          className
+                        }) {
+
+    const [isHover, setIsHover] = useState(false);
+
+    return (
+        <>
+            <div
+                className={style[className]}
+                onMouseEnter={() => setIsHover(true)}
+                onMouseLeave={() => setIsHover(false)}
+                {...(onClick && {onClick})}
+            >
+                <Tooltip label={label}>
+                    <Icon
+                        path={path}
+                        width={width}
+                        height={height}
+                        color={isHover ? {hColor} : color}
+                    />
+                </Tooltip>
+            </div>
+        </>
+    )
+}
+
+export default IconFunction;
