2e5ae6058fb8dd42a9601a4632832749c7ec8f39
Shivanshu <97258879+shivanshupareek@users.noreply.github.com>
2025-07-28 07:13:31 +0530

    Update function of MusicControls.jsx

--- src/components/Controller/MusicControls.jsx
+++ src/components/Controller/MusicControls.jsx
@@ -1,16 +1,38 @@
 import style from "../../styles/Controller/MusicControls.module.scss";
 import Progressbar from "./Progressbar.jsx";
 import CenterControls from "./CenterControls.jsx";
+import {useRef, useState, useEffect} from "react";
 
 function MusicControls() {
+    
+    const audioRef = useRef("null");
+    const [isPlaying, serIsPlaying] = useState(false);
+    const [isEnabled, setIsEnabled] = useState(false);
+    
+    function togglePlayPause() {
+        const audio = audio.current;
+        if (!audio) return ("");
+        
+        if (audio.current) {
+           setIsPlaying(true)
+            setIsEnabled(true)
+        };
+    };
+
+    
     return (
         <>
             <div className={style.music_controls}>
+                <audio 
+                    className{style.audio} 
+                    src="/assets/music.mp3" 
+                    ref{audioRef}
+                />
                 <div className={style.centerControls}>
-                    <CenterControls />
+                    <CenterControls isPlaying={isPlaying} isEnabled={isEnabled}/>
                 </div>
                 <div className={style.progressbar}>
-                    <Progressbar />
+                    <Progressbar isPlaying={isPlaying} isEnabled={isEnabled}/>
                 </div>
             </div>
         </>
