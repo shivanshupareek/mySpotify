767522cdd9c3162fa082039e5789325aeab232a9
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-07-31 19:58:09 +0530

    clearance

--- src/components/Controller/MusicControls.jsx
+++ src/components/Controller/MusicControls.jsx
@@ -1,33 +1,12 @@
 import style from "../../styles/Controller/MusicControls.module.scss";
 import Progressbar from "./Progressbar.jsx";
 import CenterControls from "./CenterControls.jsx";
-// import {useRef, useState, useEffect} from "react";
 
 function MusicControls() {
-    
-    // const audioRef = useRef("null");
-    // const [isPlaying, serIsPlaying] = useState(false);
-    // const [isEnabled, setIsEnabled] = useState(false);
-    
-    // function togglePlayPause() {
-    //     const audio = audio.current;
-    //     if (!audio) return ("");
-    //
-    //     if (audio.current) {
-    //        setIsPlaying(true)
-    //         setIsEnabled(true)
-    //     };
-    // };
-
     
     return (
         <>
             <div className={style.music_controls}>
-                {/*<audio */}
-                {/*    className{style.audio} */}
-                {/*    src="/assets/song.mp3"*/}
-                {/*    ref{audioRef}*/}
-                {/*/>*/}
                 <div className={style.centerControls}>
                     <CenterControls />
                 </div>
